94f7dd297569c4894c8222854bae7f5d1d56a426
Dennis Rassmann <showp1984@gmail.com>
2012-12-21 00:34:29 +0100

    thermal: use temperature1 sensor too The sensor1 seems to be cpu related too, and it usually shows higher temps (~2°C). Use the higher value of both to decide if thermal throttling is neccessary.
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -21,7 +21,8 @@
 #include <linux/cpu.h>
 #include <linux/reboot.h>
 
-#define DEF_TEMP_SENSOR      0
+#define DEF_TEMP_SENSOR0      0
+#define DEF_TEMP_SENSOR1      1
 
 //shutdown temp
 #define DEF_SHUTDOWNTEMP 80
@@ -111,22 +112,25 @@ static int update_cpu_max_freq(struct cpufreq_policy *cpu_policy,
 static void check_temp(struct work_struct *work)
 {
 	struct cpufreq_policy *cpu_policy = NULL;
-	struct tsens_device tsens_dev;
-	unsigned long temp = 0;
+	struct tsens_device tsens_dev0;
+	struct tsens_device tsens_dev1;
+	unsigned long temp0 = 0, temp1 = 0;
 	unsigned int max_freq = 0;
 	bool update_policy = false;
 	int cpu = 0;
-	int ret = 0;
+	int ret0 = 0, ret1 = 0;
 
-	tsens_dev.sensor_num = DEF_TEMP_SENSOR;
-	ret = tsens_get_temp(&tsens_dev, &temp);
-	if (ret) {
-		pr_err("msm_thermal: Unable to read TSENS sensor %d\n",
-				tsens_dev.sensor_num);
+	tsens_dev0.sensor_num = DEF_TEMP_SENSOR0;
+	ret0 = tsens_get_temp(&tsens_dev0, &temp0);
+	tsens_dev1.sensor_num = DEF_TEMP_SENSOR1;
+	ret1 = tsens_get_temp(&tsens_dev1, &temp1);
+	if (ret0 && ret1) {
+		pr_err("msm_thermal: FATAL: Unable to read TSENS sensor %d & %d\n",
+				tsens_dev0.sensor_num, tsens_dev1.sensor_num);
 		goto reschedule;
 	}
 
-        if (temp >= (msm_thermal_tuners_ins.shutdown_temp)) {
+        if ((max(temp0, temp1)) >= (msm_thermal_tuners_ins.shutdown_temp)) {
                 mutex_lock(&emergency_shutdown_mutex);
                 pr_warn("################################\n");
                 pr_warn("################################\n");
@@ -141,7 +145,7 @@ static void check_temp(struct work_struct *work)
                         max_freq = msm_thermal_tuners_ins.allowed_max_freq;
                         thermal_throttled = 3;
                         pr_warn("msm_thermal: Emergency throttled CPU%i to %u! temp:%lu\n",
-                                cpu, sm_thermal_tuners_ins.allowed_max_freq temp);
+                                cpu, msm_thermal_tuners_ins.allowed_max_freq, (max(temp0, temp1)));
                 }
                 mutex_unlock(&emergency_shutdown_mutex);
         }
@@ -159,15 +163,16 @@ static void check_temp(struct work_struct *work)
                         pre_throttled_max = cpu_policy->max;
 
 		//low trip point
-		if ((temp >= msm_thermal_tuners_ins.allowed_low_high) &&
-		    (temp < msm_thermal_tuners_ins.allowed_mid_high) &&
+		if (((max(temp0, temp1)) >= msm_thermal_tuners_ins.allowed_low_high) &&
+		    ((max(temp0, temp1)) < msm_thermal_tuners_ins.allowed_mid_high) &&
 		    (cpu_policy->max > msm_thermal_tuners_ins.allowed_low_freq)) {
 			update_policy = true;
 			max_freq = msm_thermal_tuners_ins.allowed_low_freq;
 			thermal_throttled = 1;
-			pr_warn("msm_thermal: Thermal Throttled (low)! temp: %lu\n", temp);
+			pr_warn("msm_thermal: Thermal Throttled (low)! temp:%lu by:%s\n",
+                                (max(temp0, temp1)), (temp0>temp1) ? "0" : "1");
 		//low clr point
-		} else if ((temp < msm_thermal_tuners_ins.allowed_low_low) &&
+		} else if (((max(temp0, temp1)) < msm_thermal_tuners_ins.allowed_low_low) &&
 			   (thermal_throttled > 0)) {
 			if (cpu_policy->max < cpu_policy->cpuinfo.max_freq) {
 				if (pre_throttled_max != 0)
@@ -180,18 +185,20 @@ static void check_temp(struct work_struct *work)
 				/* wait until 4th core is unthrottled */
 				if (cpu == 3)
 					thermal_throttled = 0;
-				pr_warn("msm_thermal: Low Thermal Throttling Ended! temp: %lu\n", temp);
+				pr_warn("msm_thermal: Low Thermal Throttling Ended! temp:%lu by:%s\n",
+                                (max(temp0, temp1)), (temp0>temp1) ? "0" : "1");
 			}
 		//mid trip point
-		} else if ((temp >= msm_thermal_tuners_ins.allowed_low_high) &&
-			   (temp < msm_thermal_tuners_ins.allowed_mid_low) &&
+		} else if (((max(temp0, temp1)) >= msm_thermal_tuners_ins.allowed_low_high) &&
+			   ((max(temp0, temp1)) < msm_thermal_tuners_ins.allowed_mid_low) &&
 			   (cpu_policy->max > msm_thermal_tuners_ins.allowed_mid_freq)) {
 			update_policy = true;
 			max_freq = msm_thermal_tuners_ins.allowed_low_freq;
 			thermal_throttled = 2;
-			pr_warn("msm_thermal: Thermal Throttled (mid)! temp: %lu\n", temp);
+			pr_warn("msm_thermal: Thermal Throttled (mid)! temp:%lu by:%s\n",
+                                (max(temp0, temp1)), (temp0>temp1) ? "0" : "1");
 		//mid clr point
-		} else if ( (temp < msm_thermal_tuners_ins.allowed_mid_low) &&
+		} else if (((max(temp0, temp1)) < msm_thermal_tuners_ins.allowed_mid_low) &&
 			   (thermal_throttled > 1)) {
 			if (cpu_policy->max < cpu_policy->cpuinfo.max_freq) {
 				max_freq = msm_thermal_tuners_ins.allowed_low_freq;
@@ -199,17 +206,19 @@ static void check_temp(struct work_struct *work)
 				/* wait until 4th core is unthrottled */
 				if (cpu == 3)
 					thermal_throttled = 1;
-				pr_warn("msm_thermal: Mid Thermal Throttling Ended! temp: %lu\n", temp);
+				pr_warn("msm_thermal: Mid Thermal Throttling Ended! temp:%lu by:%s\n",
+                                (max(temp0, temp1)), (temp0>temp1) ? "0" : "1");
 			}
 		//max trip point
-		} else if ((temp >= msm_thermal_tuners_ins.allowed_max_high) &&
+		} else if (((max(temp0, temp1)) >= msm_thermal_tuners_ins.allowed_max_high) &&
 			   (cpu_policy->max > msm_thermal_tuners_ins.allowed_max_freq)) {
 			update_policy = true;
 			max_freq = msm_thermal_tuners_ins.allowed_max_freq;
 			thermal_throttled = 3;
-			pr_warn("msm_thermal: Thermal Throttled (max)! temp: %lu\n", temp);
+			pr_warn("msm_thermal: Thermal Throttled (max)! temp:%lu by:%s\n",
+                                (max(temp0, temp1)), (temp0>temp1) ? "0" : "1");
 		//max clr point
-		} else if ((temp < msm_thermal_tuners_ins.allowed_max_low) &&
+		} else if (((max(temp0, temp1)) < msm_thermal_tuners_ins.allowed_max_low) &&
 			   (thermal_throttled > 2)) {
 			if (cpu_policy->max < cpu_policy->cpuinfo.max_freq) {
 				max_freq = msm_thermal_tuners_ins.allowed_mid_freq;
@@ -217,7 +226,8 @@ static void check_temp(struct work_struct *work)
 				/* wait until 4th core is unthrottled */
 				if (cpu == 3)
 					thermal_throttled = 2;
-				pr_warn("msm_thermal: Max Thermal Throttling Ended! temp: %lu\n", temp);
+				pr_warn("msm_thermal: Max Thermal Throttling Ended! temp:%lu by:%s\n",
+                                (max(temp0, temp1)), (temp0>temp1) ? "0" : "1");
 			}
 		}
 
