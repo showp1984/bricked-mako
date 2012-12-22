2a96bb7fef9fc639c68133ebfff8fbfe6b787587
Dennis Rassmann <showp1984@gmail.com>
2012-12-22 08:07:58 +0100

    thermal: fix hardware issue with temp sensor 1
    Ignore insane values and also ignore sensor 1 in suspend.
    
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -20,6 +20,7 @@
 #include <linux/workqueue.h>
 #include <linux/cpu.h>
 #include <linux/reboot.h>
+#include <linux/earlysuspend.h>
 
 #define DEF_TEMP_SENSOR0      0
 #define DEF_TEMP_SENSOR1      1
@@ -51,6 +52,9 @@ static int thermal_throttled = 0;
 
 //Save the cpu max freq before throttling
 static int pre_throttled_max = 0;
+
+//screen status
+static bool screen_blank = false;
 
 static struct delayed_work check_temp_work;
 static struct workqueue_struct *check_temp_workq;
@@ -118,6 +122,7 @@ static void check_temp(struct work_struct *work)
 	bool update_policy = false;
 	int i = 0, cpu = 0;
 	int ret0 = 0, ret1 = 0;
+        bool sensor_fail = false;
 
 	tsens_dev0.sensor_num = DEF_TEMP_SENSOR0;
 	ret0 = tsens_get_temp(&tsens_dev0, &temp0);
@@ -129,13 +134,20 @@ static void check_temp(struct work_struct *work)
 		goto reschedule;
 	}
 
-        temp = (max(temp0, temp1));
+        if ((screen_blank) || (temp1 < 0) || (temp1 > 150)) {
+                sensor_fail = true;
+                temp = temp0;
+        } else {
+                sensor_fail = false;
+                temp = (max(temp0, temp1));
+        }
 
         if (temp >= msm_thermal_tuners_ins.shutdown_temp) {
                 mutex_lock(&emergency_shutdown_mutex);
                 pr_warn("################################\n");
                 pr_warn("################################\n");
                 pr_warn("- %u OVERTEMP! SHUTTING DOWN! -\n", msm_thermal_tuners_ins.shutdown_temp);
+                pr_warn("- cur temp:%lu measured by:%s -\n", temp, ((sensor_fail) || (temp0>temp1)) ? "0" : "1");
                 pr_warn("################################\n");
                 pr_warn("################################\n");
                 /* orderly poweroff tries to power down gracefully
@@ -172,7 +184,7 @@ static void check_temp(struct work_struct *work)
                         if (cpu == (CONFIG_NR_CPUS-1)) {
                                 thermal_throttled = 1;
                                 pr_warn("msm_thermal: Thermal Throttled (low)! temp:%lu by:%s\n",
-                                        temp, (temp0>temp1) ? "0" : "1");
+                                        temp, ((sensor_fail) || (temp0>temp1)) ? "0" : "1");
                         }
 		//low clr point
 		} else if ((temp < msm_thermal_tuners_ins.allowed_low_low) &&
@@ -192,7 +204,7 @@ static void check_temp(struct work_struct *work)
                         if (cpu == (CONFIG_NR_CPUS-1)) {
                                 thermal_throttled = 0;
                                 pr_warn("msm_thermal: Low thermal throttle ended! temp:%lu by:%s\n",
-                                        temp, (temp0>temp1) ? "0" : "1");
+                                        temp, ((sensor_fail) || (temp0>temp1)) ? "0" : "1");
                         }
 		//mid trip point
 		} else if ((temp >= msm_thermal_tuners_ins.allowed_mid_high) &&
@@ -203,7 +215,7 @@ static void check_temp(struct work_struct *work)
                         if (cpu == (CONFIG_NR_CPUS-1)) {
                                 thermal_throttled = 2;
                                 pr_warn("msm_thermal: Thermal Throttled (mid)! temp:%lu by:%s\n",
-                                        temp, (temp0>temp1) ? "0" : "1");
+                                        temp, ((sensor_fail) || (temp0>temp1)) ? "0" : "1");
                         }
 		//mid clr point
 		} else if ((temp < msm_thermal_tuners_ins.allowed_mid_low) &&
@@ -213,7 +225,7 @@ static void check_temp(struct work_struct *work)
                         if (cpu == (CONFIG_NR_CPUS-1)) {
                                 thermal_throttled = 1;
                                 pr_warn("msm_thermal: Mid thermal throttle ended! temp:%lu by:%s\n",
-                                        temp, (temp0>temp1) ? "0" : "1");
+                                        temp, ((sensor_fail) || (temp0>temp1)) ? "0" : "1");
                         }
 		//max trip point
 		} else if (temp >= msm_thermal_tuners_ins.allowed_max_high) {
@@ -222,7 +234,7 @@ static void check_temp(struct work_struct *work)
                         if (cpu == (CONFIG_NR_CPUS-1)) {
 			        thermal_throttled = 3;
                                 pr_warn("msm_thermal: Thermal Throttled (max)! temp:%lu by:%s\n",
-                                        temp, (temp0>temp1) ? "0" : "1");
+                                        temp, ((sensor_fail) || (temp0>temp1)) ? "0" : "1");
                         }
 		//max clr point
 		} else if ((temp < msm_thermal_tuners_ins.allowed_max_low) &&
@@ -232,7 +244,7 @@ static void check_temp(struct work_struct *work)
                         if (cpu == (CONFIG_NR_CPUS-1)) {
                                 thermal_throttled = 2;
                                 pr_warn("msm_thermal: Max thermal throttle ended! temp:%lu by:%s\n",
-                                        temp, (temp0>temp1) ? "0" : "1");
+                                        temp, ((sensor_fail) || (temp0>temp1)) ? "0" : "1");
                         }
 		}
 
@@ -503,6 +515,22 @@ static struct attribute_group msm_thermal_attr_group = {
 };
 /**************************** SYSFS END ****************************/
 
+static void msm_thermal_early_suspend(struct early_suspend *h)
+{
+        screen_blank = true;
+}
+
+static void msm_thermal_late_resume(struct early_suspend *h)
+{
+        screen_blank = false;
+}
+
+static struct early_suspend msm_thermal_early_suspend_handler = {
+	.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN,
+	.suspend = msm_thermal_early_suspend,
+	.resume = msm_thermal_late_resume,
+};
+
 static int __init msm_thermal_init(void)
 {
 	int rc, ret = 0;
@@ -524,6 +552,8 @@ static int __init msm_thermal_init(void)
 		}
 	} else
 		pr_warn("msm_thermal: sysfs: ERROR, could not create sysfs kobj");
+
+        register_early_suspend(&msm_thermal_early_suspend_handler);
 
 	return ret;
 }
