ccec00b600863547f05ece2714f88c662a343ec8
Dennis Rassmann <showp1984@gmail.com>
2013-07-27 23:33:17 +0200

    cpuinit: get rid of __cpuinit, first regexp This commit is the result of:
    find -name "*.c" | xargs sed -i "s/ __cpuinit / /g"
    
    Signed-off-by: Arjan van de Ven <arjan@linux.intel.com>
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- arch/arm/mach-msm/platsmp.c
+++ arch/arm/mach-msm/platsmp.c
@@ -47,7 +47,7 @@ volatile int pen_release = -1;
  * observers, irrespective of whether they're taking part in coherency
  * or not.  This is necessary for the hotplug code to work reliably.
  */
-static void __cpuinit write_pen_release(int val)
+static void write_pen_release(int val)
 {
 	pen_release = val;
 	smp_wmb();
@@ -57,7 +57,7 @@ static void __cpuinit write_pen_release(int val)
 
 static DEFINE_SPINLOCK(boot_lock);
 
-void __cpuinit platform_secondary_init(unsigned int cpu)
+void platform_secondary_init(unsigned int cpu)
 {
 	WARN_ON(msm_platform_secondary_init(cpu));
 
@@ -81,7 +81,7 @@ void __cpuinit platform_secondary_init(unsigned int cpu)
 	spin_unlock(&boot_lock);
 }
 
-static int __cpuinit scorpion_release_secondary(void)
+static int scorpion_release_secondary(void)
 {
 	void *base_ptr = ioremap_nocache(0x00902000, SZ_4K*2);
 	if (!base_ptr)
@@ -97,7 +97,7 @@ static int __cpuinit scorpion_release_secondary(void)
 	return 0;
 }
 
-static int __cpuinit krait_release_secondary_sim(unsigned long base, int cpu)
+static int krait_release_secondary_sim(unsigned long base, int cpu)
 {
 	void *base_ptr = ioremap_nocache(base + (cpu * 0x10000), SZ_4K);
 	if (!base_ptr)
@@ -113,7 +113,7 @@ static int __cpuinit krait_release_secondary_sim(unsigned long base, int cpu)
 	return 0;
 }
 
-static int __cpuinit krait_release_secondary(unsigned long base, int cpu)
+static int krait_release_secondary(unsigned long base, int cpu)
 {
 	void *base_ptr = ioremap_nocache(base + (cpu * 0x10000), SZ_4K);
 	if (!base_ptr)
@@ -144,7 +144,7 @@ static int __cpuinit krait_release_secondary(unsigned long base, int cpu)
 	return 0;
 }
 
-static int __cpuinit krait_release_secondary_p3(unsigned long base, int cpu)
+static int krait_release_secondary_p3(unsigned long base, int cpu)
 {
 	void *base_ptr = ioremap_nocache(base + (cpu * 0x10000), SZ_4K);
 	if (!base_ptr)
@@ -169,7 +169,7 @@ static int __cpuinit krait_release_secondary_p3(unsigned long base, int cpu)
 	return 0;
 }
 
-static int __cpuinit release_secondary(unsigned int cpu)
+static int release_secondary(unsigned int cpu)
 {
 	BUG_ON(cpu >= get_core_count());
 
@@ -198,7 +198,7 @@ static int cold_boot_flags[] = {
 	SCM_FLAG_COLDBOOT_CPU3,
 };
 
-int __cpuinit boot_secondary(unsigned int cpu, struct task_struct *idle)
+int boot_secondary(unsigned int cpu, struct task_struct *idle)
 {
 	int ret;
 	unsigned int flag = 0;
