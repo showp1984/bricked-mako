16e95b99140a793ab496a2b3afccc16075028d40
Dennis Rassmann <showp1984@gmail.com>
2013-07-28 01:28:10 +0200

    clocks-8960: gpu: allow oc to 487.5 & 533.333 Mhz
    In the new version of this file QCOM tried to merge all apq8064 freq tables.
    We can't do that.
    APQ8064aa & APQ8064 share a pll freq of 975Mhz, but APQ8064ab has only 900Mhz
    on PLL15! So if we were to share one freq table, then the 450Mhz entry would in
    fact be 487.5Mhz.
    So we just create a dedicated table for apq8064&aa and use that.
    
    PS: It is probable that QCOM did this because apq8064&aa are limited to 400Mhz anyway
    and don't use the pll15 anywhere within their limits. However, this sucks for us now.
    
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -3510,7 +3510,9 @@ static struct rcg_clk gfx2d1_clk = {
 		.ctl_val = CC_BANKED(9, 6, n), \
 	}
 
-/*Shared by 8064, 8930, and 8960ab*/
+/*Shared by 8064, 8930, and 8960ab
+ * This /\ is nonsense. 8064 and 8064ab have different
+ * pll15 values! 450Mhz == 487.5Mhz on apq8064! */
 static struct clk_freq_tbl clk_tbl_gfx3d[] = {
 	F_GFX3D(        0, gnd,   0,  0),
 	F_GFX3D(  1800000, pxo,   1, 15),
@@ -3555,6 +3557,28 @@ static struct clk_freq_tbl clk_tbl_gfx3d_8960[] = {
 	F_END
 };
 
+static struct clk_freq_tbl clk_tbl_gfx3d_8064[] = {
+	F_GFX3D(        0, gnd,   0,  0),
+	F_GFX3D( 27000000, pxo,   0,  0),
+	F_GFX3D( 48000000, pll8,  1,  8),
+	F_GFX3D( 54857000, pll8,  1,  7),
+	F_GFX3D( 64000000, pll8,  1,  6),
+	F_GFX3D( 76800000, pll8,  1,  5),
+	F_GFX3D( 96000000, pll8,  1,  4),
+	F_GFX3D(128000000, pll8,  1,  3),
+	F_GFX3D(145455000, pll2,  2, 11),
+	F_GFX3D(160000000, pll2,  1,  5),
+	F_GFX3D(177778000, pll2,  2,  9),
+	F_GFX3D(200000000, pll2,  1,  4),
+	F_GFX3D(228571000, pll2,  2,  7),
+	F_GFX3D(266667000, pll2,  1,  3),
+	F_GFX3D(325000000, pll15, 1,  3),
+	F_GFX3D(400000000, pll2,  1,  2),
+	F_GFX3D(487500000, pll15, 1,  2),
+	F_GFX3D(533333000, pll2,  2,  3),
+	F_END
+};
+
 static struct clk_freq_tbl clk_tbl_gfx3d_8930ab[] = {
 	F_GFX3D(        0, gnd,   0,  0),
 	F_GFX3D( 27000000, pxo,   0,  0),
@@ -3586,7 +3610,7 @@ static unsigned long fmax_gfx3d_8064ab[VDD_DIG_NUM] = {
 static unsigned long fmax_gfx3d_8064[VDD_DIG_NUM] = {
 	[VDD_DIG_LOW]     = 128000000,
 	[VDD_DIG_NOMINAL] = 325000000,
-	[VDD_DIG_HIGH]    = 400000000
+	[VDD_DIG_HIGH]    = 533333000
 };
 
 static unsigned long fmax_gfx3d_8930[VDD_DIG_NUM] = {
@@ -6655,6 +6679,7 @@ static void __init msm8960_clock_pre_init(void)
 	 * clocks which differ between chips.
 	 */
 	if (cpu_is_apq8064() || cpu_is_apq8064aa()) {
+		gfx3d_clk.freq_tbl = clk_tbl_gfx3d_8064;
 		gfx3d_clk.c.fmax = fmax_gfx3d_8064;
 	}
 	if (cpu_is_apq8064ab()) {
